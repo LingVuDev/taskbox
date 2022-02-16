933057083646705cfa2073262aaf5aed5f518ab4
Ling Vu <m.vu@thalia.de>
2022-02-16 19:24:22 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./index.css";
+import store from "./lib/store";
+
+import { Provider } from "react-redux";
+import InboxScreen from "./components/InboxScreen";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
+  );
+}
+
+export default App;
